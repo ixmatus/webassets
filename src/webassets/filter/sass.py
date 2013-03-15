50eeeae1bae9420bfb5b5968fd9de5a509eefe09
Parnell Springmeyer <ixmatus@gmail.com>
2013-03-14 20:48:58 -0500

    HOKAY THIS IS THE EARTH

--- src/webassets/filter/sass.py
+++ src/webassets/filter/sass.py
@@ -133,6 +133,9 @@ class Sass(Filter):
                                     # shell: necessary on windows to execute
                                     # ruby files, but doesn't work on linux.
                                     shell=(os.name == 'nt'))
+            with open("/alfred/WTF", "w") as f:
+                f.write(bytes(_in, "utf-8"))
+            
             stdout, stderr = proc.communicate(bytes(_in, 'utf-8'))
 
             if proc.returncode != 0:
